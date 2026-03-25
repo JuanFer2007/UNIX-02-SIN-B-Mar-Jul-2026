0a3551d1705368da0c8b4745d7cfbd7522729eda
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-25 23:33:07 +0000

    Imports a key from the file

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -1,5 +1,5 @@
 gpg --list-secret-keys --keyid-format=long
 gpg --armor --export-secret-keys
 gpg --armor --export
- gpg --import Llave_de_Andy.asc
+ gpg --import andy_llave_publica.asc #Imports a key from the file
 gpg --armor --export #Exports ALL your public keys in ASCII-armored format.
