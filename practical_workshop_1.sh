d3a6aebc064271de103ac04b42ea4a0c9717c9d1
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-25 23:23:23 +0000

    Exports ALL your public keys in ASCII-armored format.

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -2,3 +2,4 @@ gpg --list-secret-keys --keyid-format=long
 gpg --armor --export-secret-keys
 gpg --armor --export
  gpg --import Llave_de_Andy.asc
+gpg --armor --export #Exports ALL your public keys in ASCII-armored format.
