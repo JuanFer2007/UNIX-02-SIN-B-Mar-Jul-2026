0d6b795b8e82863bc88107eb36796ee072f56f26
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-25 23:52:29 +0000

    Creates a plain text file with the message "Vamos al Futbol"

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,2 +1,3 @@
 gpg --armor --export #Exports ALL your public keys in ASCII-armored format.
 gpg --import andy_llave_publica.asc #Imports a key from the file
+echo "Vamos al Futbol" > doc_no_cifrado.txt #Creates a plain text file with the message "Vamos al Futbol"
