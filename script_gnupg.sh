20cc50301ef58a54ab2b4abc948009ef5c739046
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-26 00:00:05 +0000

    Encrypts doc_no_cifrado.txt for a specific recipient, saving as doc_cifrado.txt.

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,3 +1,5 @@
 gpg --armor --export #Exports ALL your public keys in ASCII-armored format.
 gpg --import andy_llave_publica.asc #Imports a key from the file
 echo "Vamos al Futbol" > doc_no_cifrado.txt #Creates a plain text file with the message "Vamos al Futbol"
+cat doc_no_cifrado.txt #Displays the content of the file doc_no_cifrado.txt on screen.
+gpg --output doc_cifrado.txt --encrypt --recipient FD999E623F0E6A05942CE911A91C2A6252985C72 doc_no_cifrado.txt # Encrypts doc_no_cifrado.txt for a specific recipient, saving as doc_cifrado.txt.
