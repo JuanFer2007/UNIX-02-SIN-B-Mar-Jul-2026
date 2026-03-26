e2bc9175e5e12fba791faac8bb74b3a31b34f612
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-26 00:39:04 +0000

    Creates BINARY signed file (original + signature combined, encrypted format).

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -9,3 +9,6 @@ cat doc_no_cifrado_firmado.txt #Displays the content of your signed file to veri
 gpg --verify MICOMPA_doc_no_cifrado_firmado.txt #Verifies the digital signature on the clearsigned file to prove authenticity and integrity.
 gpg --edit-key FD999E623F0E6A05942CE911A91C2A6252985C72 #nteractive key management for the specific key with ID FD999E623F0E6A05942CE911A91C2A6252985C72.
 gpg --sign-key A91C2A6252985C72 #Signs another person's public key with your private key to certify trust.
+gpg --verify andy_doc_no_cifrado_firmado.txt #Verifies Andy's digital signature on the clearsigned file.
+gpg --output doc_no_cifrado_firmado_binario.txt --sign doc_no_cifrado.txt #Creates BINARY signed file (original + signature combined, encrypted format).
+
