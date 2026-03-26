ca76fb6935cd39cdb3df19818423eee0e81f6ed3
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-26 01:11:05 +0000

    gpg

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -1,5 +1,5 @@
 gpg --list-secret-keys --keyid-format=long #Lists all your private (secret) keys in your GPG keyring.
 gpg --armor --export-secret-keys # Exports ALL your private keys in ASCII-armored format (text file).
-gpg --armor --export #
+gpg --armor --export #Exports ALL your public keys in ASCII-armored format.
  gpg --import andy_llave_publica.asc #
 gpg --armor --export  #
