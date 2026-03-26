c10f0798053ce71fec37153fb0ac614c044b2c0c
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-26 00:20:46 +0000

    Creates a digital signature of the file while keeping original text readable.

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -4,3 +4,4 @@ echo "Vamos al Futbol" > doc_no_cifrado.txt #Creates a plain text file with the 
 cat doc_no_cifrado.txt #Displays the content of the file doc_no_cifrado.txt on screen.
 gpg --output doc_cifrado.txt --encrypt --recipient FD999E623F0E6A05942CE911A91C2A6252985C72 doc_no_cifrado.txt # Encrypts doc_no_cifrado.txt for a specific recipient, saving as doc_cifrado.txt.
 gpg --decrypt andy_doc_cifrado.txt # Decrypts the encrypted file andy_doc_cifrado.txt and displays content on screen.
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #Creates a digital signature of the file while keeping original text readable.
