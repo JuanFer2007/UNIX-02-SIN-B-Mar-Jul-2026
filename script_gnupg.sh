8ecf6ff61cc958272a66e1fbcb51bf7873916656
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-26 00:05:16 +0000

    Decrypts the encrypted file andy_doc_cifrado.txt and displays content on screen.

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -3,3 +3,4 @@ gpg --import andy_llave_publica.asc #Imports a key from the file
 echo "Vamos al Futbol" > doc_no_cifrado.txt #Creates a plain text file with the message "Vamos al Futbol"
 cat doc_no_cifrado.txt #Displays the content of the file doc_no_cifrado.txt on screen.
 gpg --output doc_cifrado.txt --encrypt --recipient FD999E623F0E6A05942CE911A91C2A6252985C72 doc_no_cifrado.txt # Encrypts doc_no_cifrado.txt for a specific recipient, saving as doc_cifrado.txt.
+gpg --decrypt andy_doc_cifrado.txt # Decrypts the encrypted file andy_doc_cifrado.txt and displays content on screen.
