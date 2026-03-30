88ac3f38b87311ec87686403a94f6157bae4dcad
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-30 13:14:18 +0000

    Installs the parted package - a powerful disk partitioning tool

--- install_parted.sh
+++ install_parted.sh
@@ -1,4 +1,4 @@
 sudo apt update #Updates package lists (refreshes software catalog)
 sudo apt upgrade #Upgrades all installed packages to latest versions 
-sudo apt install parted #
+sudo apt install parted # Installs the parted package - a powerful disk partitioning tool
 sudo parted -l && lsblk -f && echo -e "\n---\n" #
