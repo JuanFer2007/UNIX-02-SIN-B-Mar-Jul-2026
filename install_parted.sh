7c9e73c4f43727072b50892caacd250b2ef80453
JuanFer2007 <jupindore@uide.edu.ec>
2026-03-30 13:48:39 +0000

    Shows EVERYTHING about the file archivo.txt from the filesystem perspective.

--- install_parted.sh
+++ install_parted.sh
@@ -3,4 +3,5 @@ sudo apt upgrade #Upgrades all installed packages to latest versions
 sudo apt install parted # Installs the parted package - a powerful disk partitioning tool
 sudo parted -l && lsblk -f && echo -e "\n---\n" #Comprehensive disk analysis - runs 3 commands in sequence
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #This is a one-liner shell command that detects your firmware type (UEFI vs Legacy BIOS).
- echo "esto es un archivo" > archivo.txt #Creates (or overwrites) a file called archivo.txt containing the text "esto es un archivo"
+echo "esto es un archivo" > archivo.txt #Creates (or overwrites) a file called archivo.txt containing the text "esto es un archivo"
+stat archivo.txt #Shows EVERYTHING about the file archivo.txt from the filesystem perspective.
