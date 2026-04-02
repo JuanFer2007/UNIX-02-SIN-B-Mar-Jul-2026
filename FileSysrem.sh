33a90c75995d34bef99fbdc8b9e49ae0d9599bf8
JuanFer2007 <jupindore@uide.edu.ec>
2026-04-02 02:56:52 +0000

    print working directory

--- FileSysrem.sh
+++ FileSysrem.sh
@@ -2,3 +2,4 @@ gcc -o saludo saludo1.c #Compile
 sudo cp saludo /bin/ #Install global
 sudo chmod +x /bin/saludo # Makes saludo executable by everyone
 saludo # Test global
+pwd #print working directory
