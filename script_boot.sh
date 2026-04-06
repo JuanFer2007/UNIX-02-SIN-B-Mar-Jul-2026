4b5abc1c2678035e7488265213624c6ac551e0f3
JuanFer2007 <jupindore@uide.edu.ec>
2026-04-06 12:39:46 +0000

    Create a new folder called "project" in the current directory

--- script_boot.sh
+++ script_boot.sh
@@ -3,3 +3,6 @@ cd / #Switch to the root directory (/) - the system's main folder
 cd /home/codespace #Change to the specific directory /home/codespace
 cd ~ #Change to the user's HOME directory (~ = HOME)
 cd $Home #Change to the HOME directory using the $HOME environment variable
+mkdir proyecto #Create a new folder called "project" in the current directory
+cd proyecto/ #
+las -lai #
