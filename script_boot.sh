2ea7607fad9b70716e64793a4553303ef5c23e7d
JuanFer2007 <jupindore@uide.edu.ec>
2026-04-06 12:35:22 +0000

    Change to the HOME directory using the $HOME environment variable

--- script_boot.sh
+++ script_boot.sh
@@ -2,4 +2,4 @@ cd #Goes to your HOME directory
 cd / #Switch to the root directory (/) - the system's main folder
 cd /home/codespace #Change to the specific directory /home/codespace
 cd ~ #Change to the user's HOME directory (~ = HOME)
-cd $Home #
+cd $Home #Change to the HOME directory using the $HOME environment variable
