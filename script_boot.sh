5ec6cc619aa88ae4b774654f3c1ecfd565df2a73
JuanFer2007 <jupindore@uide.edu.ec>
2026-04-06 12:40:53 +0000

    925544 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:36 . 918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:36 ..

--- script_boot.sh
+++ script_boot.sh
@@ -5,4 +5,5 @@ cd ~ #Change to the user's HOME directory (~ = HOME)
 cd $Home #Change to the HOME directory using the $HOME environment variable
 mkdir proyecto #Create a new folder called "project" in the current directory
 cd proyecto/ #Change to the newly created "project" directory (the / is optional)
-las -lai #
+las -lai #925544 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:36 .
+         #918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:36 ..
