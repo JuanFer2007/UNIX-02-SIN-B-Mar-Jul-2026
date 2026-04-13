d21767dd6afda341119f7b9bc1b3a2db528605ce
JuanFer2007 <jupindore@uide.edu.ec>
2026-04-13 12:47:25 +0000

    lsintaxis

--- script.sh
+++ script.sh
@@ -3,3 +3,7 @@ ls --all #It does exactly the same thing, but it's more explicit.
 ls #The binary/command
 -- #Option prefixes
 a-all #The argument
+ls -h # List names only (human-readable flag is ignored without -l)
+ls -l -a -h # List all files in long format with human-readable sizes (verbose)
+ls -l -ah # List all files in long format with human-readable sizes (semi-grouped)
+ls -lah #List all files, hidden included, in long format and human sizes (pro)
