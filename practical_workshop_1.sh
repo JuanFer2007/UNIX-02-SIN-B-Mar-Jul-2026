c6a111643e6df9d5bbfc4b48b1884cbf5a2b7fe5
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-03 23:50:37 +0000

    List the contents of the current directory

--- practical_workshop_1.sh
+++
@@ -1,5 +0,0 @@
-gpg --list-secret-keys --keyid-format=long #Lists all your private (secret) keys in your GPG keyring.
-gpg --armor --export-secret-keys # Exports ALL your private keys in ASCII-armored format (text file).
-gpg --armor --export #Exports ALL your public keys in ASCII-armored format.
- gpg --import andy_llave_publica.asc #
-gpg --armor --export  #
---
+++ script_linux_cisco_1.sh
@@ -0,0 +1,1 @@
+ls #List the contents of the current directory
