997b24dc09057b4aa3672ce4ab5eaca5345469c0
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 02:06:28 +0000

    Run the hello.sh script and display the "Hello World" message with the penguin

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -38,5 +38,7 @@ ls -l hello.sh  # Now this command will work and show the file details
 chmod u+x hello.sh  # Grant execution permission only to the owner (user) of the file
 ls -l hello.sh  # Verify that the execution permission 'x' was successfully added to the owner
 echo -e '#!/bin/bash\necho "----------------"\necho "( Hello World! )"\necho "----------------"\necho " \\ "\necho " \\ "\necho " <(^)"\necho " ( )"' > hello.sh  # Use echo to write the script header (shebang) and ASCII art into hello.sh
+./hello.sh  # Run the hello.sh script and display the "Hello World" message with the penguin
+
 
 
