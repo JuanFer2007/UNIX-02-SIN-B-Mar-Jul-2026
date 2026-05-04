39b8bc9b3ac8535277ab9ce3dc526daefcc8a55b
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 02:24:50 +0000

    Display the full alphabet list to compare with head and tail commands

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -46,4 +46,5 @@ ls -l hello.sh  # Verify that the file owner has been successfully changed to ro
 sudo ./hello.sh  # Execute the script with root privileges using sudo
 echo -e "1 retriever\n2 badger\n3 bat\n4 wolf\n5 eagle" > animals.txt  # Create the animals.txt file with a list of animals
 cat animals.txt  # Display the content of the animals.txt file in the terminal
+cat alpha.txt  # Display the full alphabet list to compare with head and tail commands
 
