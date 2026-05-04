81334e5af13df08b5447b8dbd7f8772948605f92
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 04:06:08 +0000

    Resynchronize the package index files from their sources

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -111,3 +111,4 @@ sudo apt-get install cowsay -y  # Download and install the cowsay utility (added
 
 # Correct way to run it in this environment using the absolute path:
 /usr/games/cowsay 'NDG Linux Unhatched'  # Run cowsay using its full path to see the cow message
+sudo apt-get update  # Resynchronize the package index files from their sources
