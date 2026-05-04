8e08075a5668edd1cb1f2a3ca1f042b4ff89c0f9
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 03:08:52 +0000

    Search for any sequence of exactly four characters

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -81,4 +81,5 @@ echo "Roses are red" > red.txt  # Create red.txt to practice output redirection
 cat red.txt                     # Display the contents of red.txt
 grep 'r..f' red.txt  # Search for a 4-letter pattern starting with 'r', ending with 'f', and any two characters in between
 grep 'r..d' red.txt  # Search for a 4-character pattern starting with 'r' and ending with 'd'
+grep '....' red.txt  # Search for any sequence of exactly four characters
 
