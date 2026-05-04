f531e6200b476f53ee480ad04d341bdfbf426b09
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 00:08:07 +0000

    List directory contents using the long listing format,This shows permissions, owner, size, and modification date

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,3 +2,4 @@ ls #List the contents of the current directory
 ls Documents #List the contents of a specific directory (documents)
 sudo apt update && sudo apt install aptitude -y # Install the aptitude package manager automatically (-y)
 aptitude moo # Execute the aptitude moo command to find hidden Easter Eggs
+ls -l # List directory contents using the long listing format,This shows permissions, owner, size, and modification date
