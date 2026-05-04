1311cc14ce96a425c6bf2eac5848606d0901fcdb
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 00:12:06 +0000

    List directory contents in reverse alphabetical order

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -3,3 +3,4 @@ ls Documents #List the contents of a specific directory (documents)
 sudo apt update && sudo apt install aptitude -y # Install the aptitude package manager automatically (-y)
 aptitude moo # Execute the aptitude moo command to find hidden Easter Eggs
 ls -l # List directory contents using the long listing format,This shows permissions, owner, size, and modification date
+ls -r # List directory contents in reverse alphabetical order
