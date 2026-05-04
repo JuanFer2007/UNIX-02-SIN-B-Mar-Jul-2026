b2538177c1647026eb3f886a4ee7b3613ecebcf4
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 13:41:14 +0000

    cat

--- security.sh
+++ security.sh
@@ -3,3 +3,6 @@ cat /etc/passwd | head -10 # Reads the user account file (/etc/passwd) and displ
  cat /etc/group | head -10 # Reads the groups file and displays the first 10 lines
 groups                    # Lists all the groups the current user belongs to
 groups $USER              # Shows the groups for the specific username stored in the $USER variable
+cat /etc/group | grep users # Filters the groups file to show only lines containing "users"
+cat /etc/group              # Displays the entire list of groups defined in the system
+cat /etc/gshadow            # Displays the secure group file (contains encrypted group passwords)
