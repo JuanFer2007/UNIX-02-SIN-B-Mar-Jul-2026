5bfd68559ab178c70910bbe8aabfdf2500dec03d
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 02:52:03 +0000

    touch linux.txt  # Create a temporary file to demonstrate the remove command rm linux.txt     # Remove the file linux.txt from the current directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -64,3 +64,5 @@ touch numbers.txt letters.txt alpha.txt     # Ensure all three files exist befor
 mkdir -p School                             # Create the School directory if it doesn't exist
 mv numbers.txt letters.txt alpha.txt School # Move multiple files into the School directory at once
 ls  # List contents to confirm that the moved files are no longer in the current directory
+touch linux.txt  # Create a temporary file to demonstrate the remove command
+rm linux.txt     # Remove the file linux.txt from the current directory
