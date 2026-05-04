517071ff7eb5e642d4d851b4072f6ebb360314aa
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 00:18:20 +0000

    List directory contents in long format and in reverse order,This combines two separate flags: -l and -r

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -4,3 +4,4 @@ sudo apt update && sudo apt install aptitude -y # Install the aptitude package m
 aptitude moo # Execute the aptitude moo command to find hidden Easter Eggs
 ls -l # List directory contents using the long listing format,This shows permissions, owner, size, and modification date
 ls -r # List directory contents in reverse alphabetical order
+ls -l -r # List directory contents in long format and in reverse order,This combines two separate flags: -l and -r
