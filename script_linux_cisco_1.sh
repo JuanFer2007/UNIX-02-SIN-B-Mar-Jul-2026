88c8ecc2329b61bb90d7fc02ad0098b2b2963c8f
JuanFer2007 <jupindore@uide.edu.ec>
2026-05-04 00:27:17 +0000

    Further increase verbosity (-vv) to continue the search for Easter Eggs,The program starts to get annoyed with our persistence

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -7,3 +7,4 @@ ls -r # List directory contents in reverse alphabetical order
 ls -l -r # List directory contents in long format and in reverse order,This combines two separate flags: -l and -r
 ls -rl # List directory contents in long format and reverse order using combined flags,Note: The order of flags doesn't matter (e.g., -rl is the same as -lr)
 aptitude -v moo # Increase verbosity (-v) to ask aptitude again for Easter Eggs,This is part of a progressive joke in the aptitude package manager
+aptitude -vv moo # Further increase verbosity (-vv) to continue the search for Easter Eggs,The program starts to get annoyed with our persistence
