60becfe12452e4982f3d96c9bba4a654a3baea14
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-01 13:44:51 +0000

    root_echo

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -8,4 +8,11 @@ echo "This book's name is ${book}"
 #This book's name is black hat bash
 root_directory=$(ls -ld /)
 echo "${root_directory}"
-
+#$ bash -x blackhatbash3.sh
+#+ book='black hat bash'
+#+ echo 'This book'\''s name is black hat bash'
+#This book's name is black hat bash
+#++ ls -ld /
+#+ root_directory='drwxr-xr-x 1 root root 4096 jun  1 12:48 /'
+#+ echo 'drwxr-xr-x 1 root root 4096 jun  1 12:48 /'
+#drwxr-xr-x 1 root root 4096 jun  1 12:48 /
