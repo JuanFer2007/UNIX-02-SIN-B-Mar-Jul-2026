33a9d6714665401be2bee352ea0916d0ae5ae8c8
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-01 13:42:20 +0000

    mkdir_biencomentado

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -2,7 +2,10 @@
 book="black hat bash"
 echo "This book's name is ${book}"
 
-[Juan-Pindo] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ #bash -x blackhatbash3.sh
-+ book='black hat bash'
-+ echo 'This book'\''s name is black hat bash'
-This book's name is black hat bash
+#[Juan-Pindo] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ #bash -x blackhatbash3.sh
+#+ book='black hat bash'
+#+ echo 'This book'\''s name is black hat bash'
+#This book's name is black hat bash
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+
