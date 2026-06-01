0a3a0d750dfa0c5c0e2fa2ce12ee8d1587aacc44
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-01 13:46:43 +0000

    book_echo_

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -16,3 +16,17 @@ echo "${root_directory}"
 #+ root_directory='drwxr-xr-x 1 root root 4096 jun  1 12:48 /'
 #+ echo 'drwxr-xr-x 1 root root 4096 jun  1 12:48 /'
 #drwxr-xr-x 1 root root 4096 jun  1 12:48 /
+book="Black Hat Bash"
+unset book
+echo "${book}"
+#[Juan-Pindo] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ bash -x blackhatbash3.sh
+#+ book='black hat bash'
+#+ echo 'This book'\''s name is black hat bash'
+#This book's name is black hat bash
+#++ ls -ld /
+#+ root_directory='drwxr-xr-x 1 root root 4096 jun  1 12:48 /'
+#+ echo 'drwxr-xr-x 1 root root 4096 jun  1 12:48 /'
+#drwxr-xr-x 1 root root 4096 jun  1 12:48 /
+#+ book='Black Hat Bash'
+#+ unset book
+#+ echo ''
