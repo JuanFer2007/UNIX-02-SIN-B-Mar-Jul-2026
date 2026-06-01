ed72340f5775b5c124e4aeb1b7f22f0afccd24f4
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-01 13:07:05 +0000

    bin/bash/script

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 bash --version
 env
 echo ${SHELL}
@@ -7,3 +9,5 @@ echo ${OSTYPE}
 ps -e -f
 ps -ef
 df --human-readable
+#!/bin/bash -r # Shebang with restricted mode (-r): executes the script using Bash in a secure environment that locks down dangerous actions like changing directories or modifying system variables.
+#!/bin/bash -x # Shebang with debug mode (-x): executes the script using Bash while printing every command and its arguments to the screen as they run.
