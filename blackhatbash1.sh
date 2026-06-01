0924c0bae0faee6158620d06a131d30424f26978
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-01 13:10:23 +0000

    #!/bin/bash-n

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -11,3 +11,4 @@ ps -ef
 df --human-readable
 #!/bin/bash -r # Shebang with restricted mode (-r): executes the script using Bash in a secure environment that locks down dangerous actions like changing directories or modifying system variables.
 #!/bin/bash -x # Shebang with debug mode (-x): executes the script using Bash while printing every command and its arguments to the screen as they run.
+#!/bin/bash-n blackhatbash1.sh # Syntax check mode (-n): reads the script to check for syntax errors without executing any of its commands.
