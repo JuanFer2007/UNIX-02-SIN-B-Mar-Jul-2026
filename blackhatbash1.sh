e1585d1ca47df4e9a666593520192584e7b2b0ae
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-01 13:18:11 +0000

    set-x_set+x_script

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 bash --version
 env
 echo ${SHELL}
@@ -12,3 +12,4 @@ df --human-readable
 #!/bin/bash -r # Shebang with restricted mode (-r): executes the script using Bash in a secure environment that locks down dangerous actions like changing directories or modifying system variables.
 #!/bin/bash -x # Shebang with debug mode (-x): executes the script using Bash while printing every command and its arguments to the screen as they run.
 #!/bin/bash-n blackhatbash1.sh # Syntax check mode (-n): reads the script to check for syntax errors without executing any of its commands.
+set +x
