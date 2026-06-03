8ecc3f9aec90b7342dded34a7245e161722d9079
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-03 13:34:12 +0000

    Redirection Operators The three standard streams we highlighted earlier can be redirected from one program to another

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -3,3 +3,25 @@ touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "El comando lzl fallo"
+echo "uno" > archivo.txt
+cat archivo.txt
+
+echo "uno-uno" > archivo.txt
+cat archivo.txt
+
+echo "uno-uno-dos" >> archivo.txt
+cat archivo.txt
+
+echo "tres" &> archivo.txt
+cat archivo.txt
+
+echo "tres" &>> archivo.txt
+cat archivo.txt
+
+ls -l / &> stdout_and_stderr.txt
+cat > stdout_and_stderr.txt
+
+ls -l / 1> stdout.txt 2> stderr.txt
+
+lzl 2> error.txt
+cat error.txt
