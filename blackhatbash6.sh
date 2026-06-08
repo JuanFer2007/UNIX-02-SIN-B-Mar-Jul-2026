ff5e86168091ab9781b98661e3832147eb2e01af
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-08 12:43:56 +0000

    # This script will ping any address provided as an argument.

---
+++ blackhatbash6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ls -l / | grep "bin"
+
+# This script will ping any address provided as an argument.
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target: ${TARGET}..."
+ping "${TARGET}"
