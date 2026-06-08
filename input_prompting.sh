8e996156cc408d5f89aecc1378304f835f67d184
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-08 13:21:46 +0000

    # Experimenting with exit codes

--- input_prompting.sh
+++ input_prompting.sh
@@ -7,3 +7,13 @@ echo "What is your last name?"
 read -r lastname
 echo "Your first name is ${firstname} and your last name is
 ${lastname}"
+
+# Experimenting with exit codes
+ls -l > /dev/null
+echo "The exit code of the ls command was: $?"
+lzl 2> /dev/null
+echo "The exit code of the non-existing lzl command was: $?"
+
+# Sets the exit code of the script to be 223
+echo "Exiting with exit code: 223"
+exit 223
