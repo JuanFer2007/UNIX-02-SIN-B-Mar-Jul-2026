db4079a7f39cea3149e8aaac734cc0b480a45a62
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-17 12:45:52 +0000

    blackhatbashtodo

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -5,3 +5,7 @@ awk '{print $2}' log.txt
 awk '{print $3}' log.txt
 awk '{print $1,$NF}' log.txt
 awk '{print $NF}' log.txt
+awk -F',' '{print $1}' example_csv.txt
+awk 'NR < 10' log.txt
+head log.txt
+grep "42.236.10.117" log.txt | awk '{print $7}'
