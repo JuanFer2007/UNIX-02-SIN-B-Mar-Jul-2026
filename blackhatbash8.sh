63e77c7eaf73fb23e45a6105f54179df3fa08a75
JuanFer2007 <jupindore@uide.edu.ec>
2026-06-17 12:58:05 +0000

    Mozilla-Godzilla

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -7,5 +7,9 @@ awk '{print $1,$NF}' log.txt
 awk '{print $NF}' log.txt
 awk -F',' '{print $1}' example_csv.txt
 awk 'NR < 10' log.txt
-head log.txt
+head log.t
 grep "42.236.10.117" log.txt | awk '{print $7}'
+sed 's/Mozilla/Godzilla/g' log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep -o "Mozilla" log.txt       # Searches 'log.txt' and prints only the exact matches of "Mozilla" on new lines, ignoring the rest of the text.
+grep -o "Godzilla" newlog.txt   # Searches 'newlog.txt' and displays only the word "Godzilla" each time it is found, isolating it from the context.
